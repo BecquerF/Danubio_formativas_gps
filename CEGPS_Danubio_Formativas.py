913c58662348fa5024bd9e0a46ea21ed1066c9c8
BecquerF <becquerfernandezm@gmail.com>
2026-05-23 08:41:29 -0300

    vers. 1.36

--- CEGPS_Danubio_Formativas.py
+++ CEGPS_Danubio_Formativas.py
@@ -127,26 +127,27 @@ app.layout = html.Div([
                     html.Div(
                         [
                             html.Img(
-                                src="/assets/logo_dataload.png",
-                                style={
-                                    "width":"160px",
-                                    "display":"block",
-                                    "borderRadius":"18px",
-                                    "marginBottom":"12px",
-                                    "maxHeight":"150px",
-                                    "objectFit":"contain",
-                                    "backgroundColor":"rgba(13, 24, 34, 0.95)",
-                                    "boxShadow":"0 18px 40px rgba(0, 0, 0, 0.45), 0 0 0 1px rgba(137, 188, 239, 0.16)",
-                                    "border":"1px solid rgba(137, 188, 239, 0.45)"
-                                }
-                            )
+    src="/assets/logo_dataload.png",
+    style={
+        "width":"100%",
+        "display":"block",
+        "borderRadius":"18px",
+        "marginBottom":"12px",
+        "maxHeight":"150px",
+        "objectFit":"contain",
+        "backgroundColor":"rgba(13, 24, 34, 0.95)",
+        "boxShadow":"0 18px 40px rgba(0, 0, 0, 0.45), 0 0 0 1px rgba(137, 188, 239, 0.16)",
+        "border":"1px solid rgba(137, 188, 239, 0.45)"
+    }
+)
                         ],
                         style={
-                            "display":"flex",
-                            "flexDirection":"column",
-                            "gap":"12px",
-                            "width":"100%"
-                        }
+    "display":"flex",
+    "flexDirection":"column",
+    "gap":"12px",
+    "width":"100%",
+    "padding":"0px"
+}
                     ),
                     html.Div(
                         [

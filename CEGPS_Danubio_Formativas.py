5665d5c4a8afb0a0f0be1b4f0ae34b17e2f211ed
BecquerF <becquerfernandezm@gmail.com>
2026-05-27 07:15:32 -0300

    1.62

--- CEGPS_Danubio_Formativas.py
+++ CEGPS_Danubio_Formativas.py
@@ -230,7 +230,7 @@ app.layout = html.Div([
                                     "objectFit": "contain",
                                     "gap": "4px",
                                     "position": "sticky",
-                                    "left": "7px",
+                                    "left": "5px",
                                     "top": "10px",
                                     "marginBottom": "4px",
                                     "border": "1px solid ##011c24",

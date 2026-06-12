7e8e533fda7c2c7a82ba04abf16a2f5e066810ca
BecquerF <becquerfernandezm@gmail.com>
2026-06-12 07:51:22 -0300

    2.4

--- CEGPS_Danubio_Formativas.py
+++ CEGPS_Danubio_Formativas.py
@@ -1911,7 +1911,7 @@ style={
                                 date=fecha_max.date(),
                                 min_date_allowed=df["Date"].min().date(),
                                 max_date_allowed=df["Date"].max().date(),
-                                display_format="DD/MM/YYYY",
+                                display_format="D/M/YYYY",
                                 style={
                                     "width": "100%",
                                     "backgroundColor": "#011c24",
